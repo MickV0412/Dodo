b6de1fa86ce5e324ea82466e8ba9026b564f3bdf
MickV0412 <1219992@student.roc-nijmegen.nl>
2026-05-11 11:25:13 +0200

    Added fenceahead detection

--- MyDodo.java
+++ MyDodo.java
@@ -44,7 +44,7 @@ public class MyDodo extends Dodo
      *                      (an obstruction or end of worl ahead)
      */
     public boolean canMove() {
-        if ( borderAhead() ){
+        if ( borderAhead() || fenceAhead() ){
             return false;
         } else {
             return true;
